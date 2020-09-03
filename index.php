91d603fc7c63f2e6a611b0435d84ce19be89af1e
Alex TAMAYO <revoltepassive@gmail.com>
2020-09-03 14:18:59 +0200

    init commit

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<?php
+   $note_maths = 15;
+   $note_francais = 12;
+   $note_histoire_geo = 9;
+   $moyenne = ($note_maths + $note_francais + $note_histoire_geo) / 3;
+   echo 'La moyenne est de '.$moyenne.' / 20.';
+?>
+?>
+<?php
+ $prix_ht = 50;
+ $tva = 20;
+ $prix_ttc = $prix_ht * ($tva / 100);
+ echo 'Le prix TTC du produit est de '.$prix_ttc.' €.';
+?>
